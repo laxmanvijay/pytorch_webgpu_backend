e345ecd5117fc6136404346375ebfdacb041e05e
Laxman Vijay <laxmanvijay24@gmail.com>
2026-02-01 12:38:52 +0100

    removed switch based component

--- include/data_transfer/data_transfer_manager.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-class DataTransferManager {
-public:
-    void perform_aggregation_with_tcp(std::vector<float> data, int rank, int world_size);
-    void perform_aggregation_with_rdma(std::vector<float> data, int rank, int world_size);
-};
--- src/data_transfer/data_transfer_manager.cpp
+++
@@ -1,6 +0,0 @@
-#include "data_transfer/data_transfer_manager.hpp"
-
-
-void DataTransferManager::perform_aggregation_with_tcp(std::vector<float> data, int rank, int world_size) {
-    // Implement TCP-based aggregation logic here
-}
